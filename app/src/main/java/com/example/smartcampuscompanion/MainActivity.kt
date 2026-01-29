fc355306977b9b1b30779d066bc5be7905257c79
Donalyn Talingdan <talingdandonalyn00@gmail.com>
2026-01-30 00:02:39 +0800

    Creating CampusData.kt and Department.kt

--- app/src/main/java/com/example/smartcampuscompanion/MainActivity.kt
+++ app/src/main/java/com/example/smartcampuscompanion/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.smartcampuscompanion.features.dashboard.DashboardScreen
+import com.example.smartcampuscompanion.features.campusinfo.CampusInfoScreen
 import com.example.smartcampuscompanion.ui.theme.SmartCampusCompanionTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmartCampusCompanionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                val navController = rememberNavController()
+
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "dashboard"
+                    ) {
+
+                        composable("dashboard") {
+                            DashboardScreen(navController)
+                        }
+
+                        composable("campusInfo") {
+                            CampusInfoScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartCampusCompanionTheme {
-        Greeting("Android")
     }
 }
