dffb8ce11e0b679d3b73d1958e87d7b42478f61b
Donalyn Talingdan <talingdandonalyn00@gmail.com>
2026-02-01 21:30:37 +0800

    Updating LoginScreen.kt

--- app/src/main/java/com/example/smartcampuscompanion/features/auth/LoginScreen.kt
+++ app/src/main/java/com/example/smartcampuscompanion/features/auth/LoginScreen.kt
@@ -1,12 +1,24 @@
 package com.example.smartcampuscompanion.features.auth
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.smartcampuscompanion.ui.components.PrimaryButton
 
 @Composable
 fun LoginScreen(onLoginSuccess: () -> Unit) {
-    Button(onClick = onLoginSuccess) {
-        Text("Login")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        PrimaryButton(text = "Login", onClick = onLoginSuccess)
     }
 }
