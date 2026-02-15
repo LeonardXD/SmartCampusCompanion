d8f05c5a147ec99dd6fa73e438ef27dd646b72b2
dreicodebase <veronqueandrie75@gmail.com>
2026-02-15 16:17:16 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/example/smartcampuscompanion/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/local/AppDatabase.kt
@@ -1,2 +1,9 @@
 package com.example.smartcampuscompanion.data.local
+@Database(
+    entities = [AnnouncementEntity::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun announcementDao(): AnnouncementDao
+}
 
