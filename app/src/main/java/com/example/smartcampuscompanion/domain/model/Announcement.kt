bfa629db9de7be1979787d020a27891cb7b3952b
dreicodebase <veronqueandrie75@gmail.com>
2026-02-15 16:20:37 +0800

    Update Announcement.kt

--- app/src/main/java/com/example/smartcampuscompanion/domain/model/Announcement.kt
+++ app/src/main/java/com/example/smartcampuscompanion/domain/model/Announcement.kt
@@ -1,17 +1,10 @@
 package com.example.smartcampuscompanion.domain.model
-package com.example.smartcampuscompanion.data.local.entity
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "announcements")
-data class AnnouncementEntity(
-    @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+data class Announcement(
+    val id: Long,
     val title: String,
     val content: String,
     val category: String,
     val isImportant: Boolean,
-    val createdAt: Long = System.currentTimeMillis()
+    val createdAt: Long
 )
-
