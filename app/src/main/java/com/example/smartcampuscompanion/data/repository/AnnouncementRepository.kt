5f7d033cb5ba938dae6f47e5a9add6cac75d9eb9
dreicodebase <veronqueandrie75@gmail.com>
2026-02-15 16:24:05 +0800

    Update AnnouncementRepository.kt

--- app/src/main/java/com/example/smartcampuscompanion/data/repository/AnnouncementRepository.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/repository/AnnouncementRepository.kt
@@ -1,25 +1,24 @@
 package com.example.smartcampuscompanion.data.repository
+import com.example.smartcampuscompanion.data.local.dao.AnnouncementDao
+import com.example.smartcampuscompanion.domain.mapper.toDomain
+import com.example.smartcampuscompanion.domain.mapper.toEntity
+import com.example.smartcampuscompanion.domain.model.Announcement
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
 class AnnouncementRepository(
     private val dao: AnnouncementDao
 ) {
 
-    suspend fun getAllAnnouncements(): List<Announcement> =
-        dao.getAll().map { it.toDomain() }
+    fun getAnnouncements(): Flow<List<Announcement>> =
+        dao.getAllAnnouncements().map { list -> list.map { it.toDomain() } }
 
-    suspend fun addAnnouncement(title: String, content: String) {
-        dao.insert(
-            AnnouncementEntity(
-                title = title,
-                content = content,
-                category = "General",
-                isImportant = false,
-                createdAt = System.currentTimeMillis()
-            )
-        )
+    suspend fun addAnnouncement(announcement: Announcement) {
+        dao.insertAnnouncement(announcement.toEntity())
     }
 
-    suspend fun deleteAnnouncement(id: Long) {
-        dao.delete(id)
+    suspend fun deleteAnnouncement(announcement: Announcement) {
+        dao.deleteAnnouncement(announcement.toEntity())
     }
 }
 
