bdaac72d5ca763903d74f6f00adf03751578c837
dreicodebase <veronqueandrie75@gmail.com>
2026-02-15 16:22:33 +0800

    Update AnnouncementMapper.kt

--- app/src/main/java/com/example/smartcampuscompanion/domain/mapper/AnnouncementMapper.kt
+++ app/src/main/java/com/example/smartcampuscompanion/domain/mapper/AnnouncementMapper.kt
@@ -1,2 +1,10 @@
 package com.example.smartcampuscompanion.domain.mapper
 
+fun AnnouncementEntity.toDomain(): Announcement {
+    return Announcement(id, title, content, category, isImportant, createdAt)
+}
+
+fun Announcement.toEntity(): AnnouncementEntity {
+    return AnnouncementEntity(id, title, content, category, isImportant, createdAt)
+}
+
