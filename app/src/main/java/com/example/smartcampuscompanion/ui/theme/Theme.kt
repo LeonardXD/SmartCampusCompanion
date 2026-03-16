d497ed19d2560dfeaaef88b197e984e69f01728d
Donalyn Talingdan <talingdandonalyn00@gmail.com>
2026-03-16 09:30:24 +0800

    Updating Theme.kt

--- app/src/main/java/com/example/smartcampuscompanion/ui/theme/Theme.kt
+++ app/src/main/java/com/example/smartcampuscompanion/ui/theme/Theme.kt
@@ -11,12 +11,16 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+/** * Color Palette for Dark Mode
+ */
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
+/** * Color Palette for Light Mode
+ */
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
@@ -50,6 +54,7 @@ fun SmartCampusCompanionTheme(
         else -> LightColorScheme
     }
 
+    // Apply the chosen colors and typography to the Material Design system
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
