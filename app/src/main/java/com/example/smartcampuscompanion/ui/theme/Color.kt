f63e4eca0c394dc3a0042ec2f63df324d86068ed
Donalyn Talingdan <talingdandonalyn00@gmail.com>
2026-04-22 18:05:36 +0800

    Updating Color.kt

--- app/src/main/java/com/example/smartcampuscompanion/ui/theme/Color.kt
+++ app/src/main/java/com/example/smartcampuscompanion/ui/theme/Color.kt
@@ -2,11 +2,11 @@ package com.example.smartcampuscompanion.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// --- Dark Theme Palette ---
+// --- This is Dark Theme Palette ---
 // These softer, desaturated colors are easier on the eyes in dark mode.
 val Purple80 = Color(0xFFD0BCFF)
 
-// --- Light Theme Palette ---
+// --- This is Light Theme Palette ---
 // These more vibrant colors provide better contrast on white/light backgrounds.
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
