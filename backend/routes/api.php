d985e22c8ed387c473c336bbeebd29a68cd403b4
dreicodebase <veronqueandrie75@gmail.com>
2026-04-22 13:19:44 +0800

    Add Task routes with Sanctum authentication in API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
 
-// default test route (KEEP THIS)
+// default test route
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// AUTH ROUTES (ADD THIS)
+// AUTH ROUTES
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// TASK ROUTES (PROTECTED)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+});
